e310e28999d8192d4811b73508ddb44469430888
Hassan El Dada <hassan.eldada06@gmail.com>
2026-07-07 18:08:00 -0400

    Update live demo link to optionpricing.ca and add name to dashboard

--- src/engine.py
+++ src/engine.py
@@ -23,16 +23,33 @@ class Value:
         # (e.g. + '+' or '*'). Empty for values you create directly
         self._op = _op
 
+        #each value stores a function that knows how to push gradient
+        #back to its inputs. By default a value has no inputs 
+        #so its backwards step does nothing - an empty function.
+        self._backward = lambda: None
+
     def __add__(self,other):
         #Called when you write self + other
         # build a NEW value whose data is the sum, and record that it came 
         #from (self,other) via the '+' operation
         out = Value(self.data + other.data, (self,other), '+')
+
+        #Define how gradient flows backwards through THIS addition.
+        #local gradient of (a+b) w.r.t each input is 1\        def _backward():
+            self.grad += out.grad # 1*out.grad
+            other.grad +=out.grad #1*out.grad
+        out._backward = _backward #attach it to the result node
         return out
     
     def __mul__(self,other):
         #Called when you write self * other
         out = Value(self.data*other.data,(self,other), '*')
+        
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data *out.grad
+        out._backward = _backward
+            
         return out
 
     def __repr__(self):
